349074d331dc3acf62cf00f71e20206ad8a02819
hyiromori <hyiromori@gmail.com>
2019-10-13 07:54:17 +0900

    Refactor.

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 #!/usr/bin/env node
 'use strict';
 
-const path = require('path');
 const express = require('express');
 const expressWs = require('express-ws');
-const yargs = require('yargs');
+const { showUsage, filepath, port } = require('./lib/args');
 const { IndexHandler } = require('./lib/middleware_index');
 const { WebSocketHandler, sendSockets } = require('./lib/middleware_websocket');
 const { startFileWatch, getFileContent } = require('./lib/file_watcher');
 
 try {
-  const Args = yargs
-    .help('help')
-    .alias('help', 'h')
-    .option('target', { alias: 't', default: './README.md', description: 'markdown file path' })
-    .option('port', { alias: 'p', default: 34567, description: 'server port' })
-    .argv;
-
-  if (Args.target == null) {
-    throw new Error('`--target/-t` option not specified.');
-  }
-  const targetFilePath = path.isAbsolute(Args.target) ? Args.target : path.resolve(process.cwd(), Args.target);
-
-  console.log('Markdown File :', targetFilePath);
-  console.log(`Preview URL   : http://localhost:${Args.port}/`);
+  console.log('Markdown File :', filepath);
+  console.log(`Preview URL   : http://localhost:${port}/`);
 
   startFileWatch({
-    filepath: targetFilePath,
+    filepath,
     onFileChanged: ({ content }) => {
       sendSockets(content);
     },
@@ -34,11 +21,11 @@ try {
 
   const app = express();
   expressWs(app);
-  app.ws('/ws', WebSocketHandler((ws) => ws.send(getFileContent(targetFilePath))));
+  app.ws('/ws', WebSocketHandler((ws) => ws.send(getFileContent(filepath))));
   app.get('/', IndexHandler);
-  app.listen(Args.port);
+  app.listen(port);
 
 } catch (err) {
   console.error(err.message);
-  process.exit(1);
+  showUsage();
 }
