ad0fedceeec1962aaae6f90ea6d3069073c0e277
hyiromori <hyiromori@gmail.com>
2019-11-07 13:36:03 +0900

    Add opener.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
-const path = require('path');
 const express = require('express');
 const expressWs = require('express-ws');
+const opener = require('opener');
 const { showUsage, filepath, port } = require('./lib/args');
 const { MarkdownHandler } = require('./lib/middleware_markdown');
 const { WebSocketHandler, sendSockets } = require('./lib/middleware_websocket');
@@ -11,9 +11,10 @@ const { startFileWatch, getFileContent } = require('./lib/file_watcher');
 
 try {
   const rootDir = process.cwd();
+  const previewUrl = `http://localhost:${port}/`;
   console.log('Root Directory :', rootDir);
   console.log('Default File   :', filepath);
-  console.log(`Preview URL    : http://localhost:${port}/`);
+  console.log(`Preview URL    : ${previewUrl}`);
 
   startFileWatch({
     filepath: path.resolve(rootDir, filepath),
@@ -29,6 +30,8 @@ try {
   app.get(`/${filepath}`, MarkdownHandler);
   app.use(express.static(rootDir));
   app.listen(port);
+
+  opener(previewUrl);
 
 } catch (err) {
   console.error(err.message);
