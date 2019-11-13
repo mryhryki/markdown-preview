4b02344ed0d8b4136fc51e77ea2d91752e505c1e
hyiromori <hyiromori@gmail.com>
2019-11-13 17:07:48 +0900

    Support syntax highlight.

---
+++ src/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const path = require('path');
+const express = require('express');
+const expressWs = require('express-ws');
+const opener = require('opener');
+const { showUsage, filepath, port } = require('./lib/args');
+const { MarkdownHandler } = require('./lib/middleware_markdown');
+const { WebSocketHandler, sendSockets } = require('./lib/middleware_websocket');
+const { startFileWatch, getFileContent } = require('./lib/file_watcher');
+
+try {
+  const rootDir = process.cwd();
+  const previewUrl = `http://localhost:${port}`;
+  console.log('Root Directory :', rootDir);
+  console.log('Default File   :', filepath);
+  console.log(`Preview URL    : ${previewUrl}`);
+
+  startFileWatch({
+    filepath: path.resolve(rootDir, filepath),
+    onFileChanged: ({ content }) => {
+      sendSockets(content);
+    },
+  });
+
+  const app = express();
+  expressWs(app);
+  app.get('/', (_req, res) => res.redirect(filepath));
+  app.ws('/ws', WebSocketHandler((ws) => ws.send(getFileContent(path.resolve(rootDir, filepath)))));
+  app.get(`/${filepath}`, MarkdownHandler);
+  app.use(express.static(rootDir));
+  app.listen(port);
+
+  if (process.env.DO_NOT_OPEN !== 'true') {
+    opener(previewUrl);
+  }
+
+} catch (err) {
+  console.error(err.message);
+  showUsage();
+}
--- src/lib/middleware_markdown.js
+++ src/lib/middleware_markdown.js
@@ -8,16 +8,24 @@ const HTML = `
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <title>Markdown Preview</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.css">
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.16.2/build/styles/default.min.css">
+  <script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.16.2/build/highlight.min.js"></script>
 </head>
 <body style="margin: 0 auto; max-width: 882px; padding: 32px;">
   <div class="markdown-body"><div id="content"></div></div>
   <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
   <script type="text/javascript">
+    const highlight = () => {
+      document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block);
+      });
+    }
     window.onload = () => {
       const ws = new WebSocket(location.protocol.replace('http', 'ws') + '//' + location.host + '/ws');
       ws.addEventListener('message', ({ data }) => {
         if (typeof(data) !== 'string' || data.length === 0) return;
         document.getElementById('content').innerHTML = marked(data);
+        highlight();
       });
     }
   </script>
