b65db6ccb936ed4fe0dcc9ecb1db2eb5f10415cd
mryhryki <mryhryki@gmail.com>
2023-03-30 06:18:38 +0900

    Remove references to node_modules (#42)

--- src/lib/directory.js
+++ src/lib/directory.js
@@ -7,16 +7,9 @@ const projectDir = path.resolve(__dirname, "..", "..");
 const staticDir = path.resolve(projectDir, "static");
 const templateDir = path.resolve(projectDir, "template");
 
-const nodeModulesDir = path.join(projectDir, "node_modules");
-
-const markedDir = path.join(nodeModulesDir, "marked");
-const githubMarkdownDir = path.join(nodeModulesDir, "github-markdown-css");
-
 module.exports = {
   rootDir,
   projectDir,
   staticDir,
   templateDir,
-  markedDir,
-  githubMarkdownDir,
 };
