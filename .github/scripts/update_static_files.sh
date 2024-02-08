7f1dcbbca9b7b17cafb0df51c23bcd60840c3c89
mryhryki <mryhryki@gmail.com>
2024-02-09 08:42:52 +0900

    Fix .github/workflows/update_static_files.yml

--- .github/scripts/update_static_files.sh
+++ .github/scripts/update_static_files.sh
@@ -11,7 +11,7 @@ function get_latest_release_version() {
   local REPO="$2"
   curl -L --silent \
        -H "Accept: application/vnd.github+json" \
-       -H "Authorization: Bearer ${GITHUB_PERSONAL_ACCESS_TOKEN}" \
+       -H "Authorization: Bearer ${GITHUB_TOKEN}" \
        -H "X-GitHub-Api-Version: 2022-11-28" \
        "https://api.github.com/repos/${OWNER}/${REPO}/releases/latest" |
     jq -r '.tag_name' |
@@ -33,6 +33,6 @@ curl --silent -o "${CSS_DIRECTORY}/github-markdown-dark.min.css" "https://cdnjs.
 # https://www.jsdelivr.com/package/npm/highlight.js
 HIGHLIGHT_JS_VERSION="$(get_latest_release_version "highlightjs" "highlight.js")"
 echo "highlight.js version: ${HIGHLIGHT_JS_VERSION}"
-curl --silent -o "${JAVASCRIPT_DIRECTORY}/highlight.min.js" "https://cdn.jsdelivr.net/npm/highlight.js@${HIGHLIGHT_JS_VERSION}/lib/index.min.js"
+curl --silent -o "${JAVASCRIPT_DIRECTORY}/highlight.min.js" "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@${HIGHLIGHT_JS_VERSION}/build/es/highlight.min.js"
 curl --silent -o "${CSS_DIRECTORY}/highlight-github.min.css" "https://cdn.jsdelivr.net/npm/highlight.js@${HIGHLIGHT_JS_VERSION}/styles/github.min.css"
 
