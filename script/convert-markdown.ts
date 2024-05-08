6728483ef81af98eca62d9f9a6ad5725cc546f46
MORIYA Hiroyuki <mryhryki@gmail.com>
2024-05-09 07:48:52 +0900

    Support emoji on the default template (#106)
    https://github.com/mryhryki/markdown-preview/issues/104

--- script/convert-markdown.ts
+++ script/convert-markdown.ts
@@ -1,5 +1,7 @@
 import { Marked } from "marked";
 import { markedHighlight } from "marked-highlight";
+import { EmojiToken, markedEmoji } from "marked-emoji";
+import emojiLib from "emojilib";
 import hljs from "highlight.js";
 import mermaid from "mermaid";
 
@@ -14,6 +16,26 @@ export const convertMarkdownToHtml = async (element: HTMLElement, markdown: stri
       },
     }),
   );
+
+  // Emoji
+  const options = {
+    emojis: Object.entries(emojiLib).reduce(
+      (dict, [emoji, keywords]) => {
+        keywords.forEach((keyword) => {
+          if (dict[keyword] == null) {
+            dict[keyword] = emoji;
+          } else {
+            console.warn(`Duplicate emoji (${dict[keyword]}) for keyword: ${keyword} ${emoji}`);
+          }
+        });
+        return dict;
+      },
+      {} as Record<string, string>,
+    ),
+    renderer: (token: EmojiToken) => token.emoji,
+  };
+  marked.use(markedEmoji(options));
+
   const renderer = new marked.Renderer();
   const originalCode = renderer.code;
   renderer.code = (code, language, escaped) => {
