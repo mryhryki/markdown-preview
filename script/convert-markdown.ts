17965e490078fa05681774761a2abc140e4ac033
mryhryki <mryhryki@gmail.com>
2024-05-09 07:29:12 +0900

    Update script/convert-markdown.ts

--- script/convert-markdown.ts
+++ script/convert-markdown.ts
@@ -1,5 +1,8 @@
 import { Marked } from "marked";
 import { markedHighlight } from "marked-highlight";
+import { EmojiToken, markedEmoji } from "marked-emoji";
+import unicodeEmojiJson from "unicode-emoji-json/data-by-emoji.json";
+import emojiLib from "emojilib";
 import hljs from "highlight.js";
 import mermaid from "mermaid";
 
@@ -14,6 +17,26 @@ export const convertMarkdownToHtml = async (element: HTMLElement, markdown: stri
       },
     }),
   );
+
+  // Emoji
+  const options = {
+    emojis: Object.entries(emojiLib).reduce(
+      (dict, [emoji, keywords]) => {
+        keywords.forEach((keyword) => {
+          if (dict[keyword] == null) {
+            dict[keyword] = emoji;
+          } else {
+            console.warn(`Duplicate emoji (${dict[keyword]}) for keyword: ${keyword} ${emoji}`);
+          }
+        });
+        return dict;
+      },
+      {} as Record<string, string>,
+    ),
+    renderer: (token: EmojiToken) => token.emoji,
+  };
+  marked.use(markedEmoji(options));
+
   const renderer = new marked.Renderer();
   const originalCode = renderer.code;
   renderer.code = (code, language, escaped) => {
