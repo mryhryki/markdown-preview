5dc8499b90cd236e6e5d2dd9708e45dae5ca2abb
mryhryki <mryhryki@gmail.com>
2024-05-08 20:34:41 +0900

    Update static/markdown-preview-websocket.js

--- static/markdown-preview-websocket.js
+++ static/markdown-preview-websocket.js
@@ -1,12 +1,12 @@
-const connectMarkdownPreview = (onMarkdownFileChanged) => {
-  const protocol = location.protocol.replace('http', 'ws');
+export const connectMarkdownPreview = (onMarkdownFileChanged) => {
+  const protocol = location.protocol.replace("http", "ws");
   const url = `${protocol}//${location.host}/ws?path=${encodeURIComponent(location.pathname)}`;
   const ws = new WebSocket(url);
-  ws.addEventListener('message', ({ data }) => {
+  ws.addEventListener("message", ({ data }) => {
     try {
       const payload = JSON.parse(data);
-      if (!('markdown' in payload)) return;
-      if (typeof (payload.markdown) !== 'string' || payload.markdown.length === 0) return;
+      if (!("markdown" in payload)) return;
+      if (typeof payload.markdown !== "string" || payload.markdown.length === 0) return;
       onMarkdownFileChanged(payload);
     } catch (err) {
       console.error(err);
