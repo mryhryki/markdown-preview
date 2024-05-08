93845a75216cb9bfbc9f1f98df1702cbc04129b8
mryhryki <mryhryki@gmail.com>
2024-05-08 21:18:08 +0900

    Update script/convert-markdown.ts

--- script/convert-markdown.ts
+++ script/convert-markdown.ts
@@ -1,6 +1,18 @@
-import { marked } from "marked";
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from "highlight.js";
 
 export const convertMarkdownToHtml = async (markdown: string): Promise<string> => {
+  // https://github.com/markedjs/marked-highlight?tab=readme-ov-file#usage
+  const marked = new Marked(
+    markedHighlight({
+      langPrefix: "hljs language-",
+      highlight(code, lang /* , info */) {
+        const language = hljs.getLanguage(lang) ? lang : "plaintext";
+        return hljs.highlight(code, { language }).value;
+      },
+    }),
+  );
   const renderer = new marked.Renderer();
   const originalCode = renderer.code;
   renderer.code = (code, language, escaped) => {
