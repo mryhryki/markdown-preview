7b27c48ae2135d54cf529a202795650b18c06ba2
mryhryki <mryhryki@gmail.com>
2024-09-12 15:07:54 +0900

    Update src/markdown.ts

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,13 +1,18 @@
-import { NextFunction, Request, Response } from "express";
-import path from "path";
+import fs from "node:fs/promises";
+import path from "node:path";
+import type { NextFunction, Request, Response } from "express";
 import { rootDir } from "./lib/directory";
 import { existsFile } from "./lib/file";
 
 export function MarkdownHandler(template: string) {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const filepath = path.resolve(rootDir, decodeURIComponent(req.path.substr(1)));
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const filepath = path.resolve(
+      rootDir,
+      decodeURIComponent(req.path.substring(1)),
+    );
     if (existsFile(filepath)) {
-      res.sendFile(template);
+      const templateContent = await fs.readFile(template, "utf-8");
+      res.status(200).send(templateContent);
     } else {
       next();
     }
